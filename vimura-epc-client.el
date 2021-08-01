3b8b91cae0ae0d70327afa071431b67e3a885e30
Daniel Nicolai <dalanicolai@gmail.com>
2021-08-01 14:01:48 +0200

    Implement rudimentary scrolling and caching
    First steps to get scrolling to work. Needs work on 'boundary conditions' (i.e.
    deal with first/last page). Implements very basic cache (without pages limit).
    
    Test with `M-x find-file-literally` some pdf, and then `M-x vimura-mode`.

--- vimura-epc-client.el
+++ vimura-epc-client.el
@@ -11,11 +11,15 @@
 (defun vimura-test (page)
   (epc:call-sync my-epc 'test (list page)))
 
+(defun vimura-init ()
+  (epc:call-sync my-epc 'vimura_init (list (buffer-file-name))))
+
 (defun vimura-page-svg-group (page &optional transform-height)
+  "Retrieve "
   (let ((width (x-display-pixel-width)))
     (with-temp-buffer
       (save-excursion
-        (insert (epc:call-sync my-epc 'echo (list page width))))
+        (insert (epc:call-sync my-epc 'page_svg (list page width))))
       ;; don't jump to beginning if not first page
       (search-forward-regexp "height=\"\\([0-9]*\\)")
       (let ((height (string-to-number (match-string 1))))
@@ -23,16 +27,17 @@
         (when transform-height
           (insert (format "transform='translate(0,%s)' " transform-height)))
         (let ((group-start (match-beginning 0)))
-          (search-forward "</g>")
+          (goto-char (point-max))
+          (search-backward "</g>")
           (cons height (buffer-substring group-start (match-end 0))))))))
 
-(defun vimura-page-triplet-image-object ()
+(defun vimura-page-triplet-image-object (page)
   (with-temp-buffer
-    (let* ((top (vimura-page-svg-group 26))
+    (let* ((top (vimura-page-svg-group (1- page)))
            (middle-start-height (car top))
-           (middle (vimura-page-svg-group 27 middle-start-height))
+           (middle (vimura-page-svg-group page middle-start-height))
            (bottom-start-height (+ (car top) (car middle)))
-           (bottom (vimura-page-svg-group 28 bottom-start-height))
+           (bottom (vimura-page-svg-group (1+ page) bottom-start-height))
            (width (x-display-pixel-width))
            (total-height (+ (car top) (car middle) (car bottom)))
            (svg (svg-image
@@ -53,3 +58,68 @@
                         bottom-start-height))
         (insert (cdr bottom))
         (create-image (buffer-string) 'svg t)))))
+
+(defun vimura-goto-page (page)
+  (remove-overlays (point-min) (point-max))
+  (let* ((triplet (if (eq page 0) 1 page))
+         (page-triplet (if-let (image-object
+                                (cdar (cl-member triplet vimura-cache :key #'car)))
+                           image-object
+                         (vimura-page-triplet-image-object triplet)))
+         (image-size (image-size page-triplet t))
+         (ol (make-overlay (point-min) (point-max))))
+    ;; (insert-image page-triplet)
+    (overlay-put ol 'display page-triplet)
+    (setq-local image-size image-size)
+    (setq-local current-triplet triplet)
+    ;; (setq-local current-page page)
+    ;; (add-to-list 'vimura-cache (cons triplet page-triplet))
+    (unless (eq page 0)
+      (image-set-window-vscroll (/ (cdr image-size) 3))
+      (image-scroll-left))))
+
+
+(defun vimura-scroll-up ()
+  (interactive)
+  (when (= (window-vscroll nil pdf-view-have-image-mode-pixel-vscroll)
+           (image-scroll-up 4))
+    (vimura-goto-page (1+ current-triplet))
+    (image-set-window-vscroll (- (cdr image-size)
+                                 (window-pixel-height)
+                                 (/ (cdr image-size) 3)))))
+
+(defun vimura-scroll-down ()
+  (interactive)
+  (when (= (window-vscroll nil pdf-view-have-image-mode-pixel-vscroll)
+           (image-scroll-down 4))
+    (vimura-goto-page (1- current-triplet))))
+
+(defun vimura-next-page ()
+  (interactive)
+  (let ((vscroll (window-vscroll nil pdf-view-have-image-mode-pixel-vscroll)))
+    (vimura-goto-page (1+ current-triplet))
+    (image-set-window-vscroll vscroll)))
+
+(defun vimura-previous-page ()
+  (interactive)
+  (let ((vscroll (window-vscroll nil pdf-view-have-image-mode-pixel-vscroll)))
+    (vimura-goto-page (1- current-triplet))
+    (image-set-window-vscroll vscroll)))
+
+(define-derived-mode vimura-mode special-mode "vimura-mode"
+  "Versatile pdf editor"
+  (vimura-init)
+  (defvar-local vimura-cache '())
+  (vimura-goto-page 0)
+  (image-mode-setup-winprops))
+
+(define-key vimura-mode-map (kbd "C-n") 'vimura-scroll-up)
+(define-key vimura-mode-map (kbd "C-p") 'vimura-scroll-down)
+
+(evilified-state-evilify-map vimura-mode-map
+  :mode vimura-mode
+  :bindings
+  "j" 'vimura-scroll-up
+  "k" 'vimura-scroll-down
+  "J" 'vimura-next-page
+  "K" 'vimura-previous-page)
--- vimura-epc-server.py
+++ vimura-epc-server.py
@@ -4,9 +4,16 @@ from epc.server import EPCServer
 
 server = EPCServer(('localhost', 0))
 
+doc = None
+
+@server.register_function
+def vimura_init(doc_file):
+    global doc
+    doc = fitz.open(doc_file)
+    return doc.metadata
+
 @server.register_function
 def test(page):
-    doc = fitz.open("/home/dalanicolai/Downloads/Coleman-Coding-Freedom.pdf")
     page = doc[page]
     pix = page.get_pixmap()
     # mag = display_width / pix.width
@@ -14,13 +21,12 @@ def test(page):
     return pix.height
 
 @server.register_function
-def echo(page, display_width):
-    doc = fitz.open("/home/dalanicolai/Downloads/Coleman-Coding-Freedom.pdf")
+def page_svg(page, display_width):
     page = doc[page]
     pix = page.get_pixmap()
-    # mag = display_width / pix.width
-    # svg = page.get_svg_image(matrix=fitz.Matrix(mag, mag))
-    svg = page.get_svg_image(matrix=fitz.Identity)
+    mag = display_width / pix.width
+    svg = page.get_svg_image(matrix=fitz.Matrix(mag, mag))
+    # svg = page.get_svg_image(matrix=fitz.Identity)
     return svg
 
 server.print_port()
